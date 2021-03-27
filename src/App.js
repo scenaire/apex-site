ff8720e9a19a4ae0539be6200a574e76bf467334
scenaire <tempesta-psyzeoul@hotmail.com>
2021-03-28 00:06:16 +0700

    learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to Elvion-Rhart';
+  const likes = 50;
+  const person = {name: 'chros', age: 25};
+
   return (
     <div className="App">
-      <p>Apex Website</p>
-      <p>I will do nav bar</p>
-      <p>but I will do Management app today haha</p>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>random : {Math.floor(Math.random() * 10 + 1)}</p>
+      </div>
     </div>
   );
 }
